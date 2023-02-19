d53fa597e322d040f3f05cf5ba4bcbae2c5d014b
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2023-02-19 17:56:39 +0000

    viewmodel setup to receive data from repository

--- app/src/main/java/com/lemzeeyyy/sayfe/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/lemzeeyyy/sayfe/viewmodels/MainActivityViewModel.kt
@@ -1,12 +1,8 @@
 package com.lemzeeyyy.sayfe.viewmodels
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.app.Application
 import android.net.Uri
-import android.provider.ContactsContract
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -17,12 +13,12 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.database.ktx.getValue
-import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import com.lemzeeyyy.sayfe.model.*
+import com.lemzeeyyy.sayfe.repository.SayfeRepository
 import kotlinx.coroutines.launch
 
 const val BUSY = 1
@@ -32,14 +28,9 @@ const val FAILED = 4
 
 class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val database = Firebase.firestore
-    private val outgoingAlertDb = Firebase.database
-    private val myRef = outgoingAlertDb.getReference("OutgoingAlerts")
-    private val collectionReference = database.collection("Guardian Angels")
-    private val userReference = database.collection("Users")
     private val fAuth = Firebase.auth
-    private var isUserRegistered = false
-
+    private var currentUser = fAuth.currentUser
+    private var currentUserid = currentUser?.uid
 
     private val _guardianLiveData = MutableLiveData<GuardianData>()
     val guardianLiveData: LiveData<GuardianData> get() = _guardianLiveData
@@ -53,23 +44,20 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
     private var _guardianAngelsStatus = MutableLiveData<Int>()
     val guardianAngelsStatus: LiveData<Int> get() = _guardianAngelsStatus
 
+    private var _incomingDataStatus = MutableLiveData<Int>()
+    val incomingDataStatus: LiveData<Int> get() = _incomingDataStatus
+
+    private var _outgoingDataStatus = MutableLiveData<Int>()
+    val outgoingDataStatus: LiveData<Int> get() = _outgoingDataStatus
+
     private val _userImageUri = MutableLiveData<Uri>()
     val userImageUri : LiveData<Uri> get() = _userImageUri
-
-    private var _currentUserCountryCode = MutableLiveData<String>()
-    val currentUserCountryCode: LiveData<String> get() = _currentUserCountryCode
 
     private var _phoneNumber = MutableLiveData<String>()
     val phoneNumber: LiveData<String> get() = _phoneNumber
 
-
-//    private val _userContactsLiveData = MutableLiveData<MutableList<RecipientContact>?>()
-//    val userContactsLiveDataList : LiveData<MutableList<RecipientContact>?> get() = _userContactsLiveData
-
-
-    private val _userContactsLiveData = MutableLiveData<ContactsState>()
-    val userContactsLiveDataList : LiveData<ContactsState> get() = _userContactsLiveData
-
+    private val _userContactsLiveData = MutableLiveData<MutableList<PhonebookContact>?>()
+    val userContactsLiveDataList : LiveData<MutableList<PhonebookContact>?> get() = _userContactsLiveData
 
     private val _outgoingAlertListLiveData = MutableLiveData<MutableList<OutgoingAlertData>?>()
     val outgoingAlertListLiveData : LiveData<MutableList<OutgoingAlertData>?> get() = _outgoingAlertListLiveData
@@ -80,43 +68,17 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
     private val _isRegistered = MutableLiveData<Boolean>()
     val isRegistered: LiveData<Boolean> get() = _isRegistered
 
-    private val _isSaved = MutableLiveData<Boolean>()
-    val isSaved: LiveData<Boolean> get() = _isSaved
+    init {
 
+        viewModelScope.launch {
 
-    fun getOutgoingAlertList(){
-        myRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                snapshot.children.forEach {
+            _userImageUri.value = currentUserid?.let { SayfeRepository.getImageUriFromDb(it) }
 
-                    val outgoingDataList = it.getValue<MutableList<OutgoingAlertData>>()!!
+           // _outgoingAlertListLiveData.value = currentUserid?.let { SayfeRepository.getOutgoingAlertList(it) }
 
-                    _outgoingAlertListLiveData.value = outgoingDataList
-                }
-            }
+           // _incomingAlertListLiveData.value = currentUserid?.let { SayfeRepository.getIncomingAlertList(it) }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.d("TAG", "onCancelled: ${error.message.toString()} ")
-            }
         }
-        )
-    }
-
-    fun getIncomingAlertList(){
-        myRef.addValueEventListener(object : ValueEventListener{
-            override fun onDataChange(snapshot: DataSnapshot) {
-                //Update recyclerView for only targeted appID
-                snapshot.children.forEach {
-                    val incomingDataList = it.getValue<MutableList<IncomingAlertData>>()!!
-                    _incomingAlertListLiveData.value = incomingDataList
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
 
     fun updateShouldNavActivity(navigate : Boolean){
@@ -124,176 +86,73 @@ class MainActivityViewModel(application: Application) : AndroidViewModel(applica
     }
 
     fun checkDuplicateRegisteredNumber(phoneNumber: String){
-        isUserRegistered = false
-        userReference.get().addOnSuccessListener { querySnapshot ->
-                querySnapshot.forEach { queryDocumentSnapshot ->
-                val users = queryDocumentSnapshot.toObject(Users::class.java)
-                    // Log.d("PHONE NUMBS", "CheckDuplicateRegisteredNumber: ${users.phoneNumber}")
-                   if (users.phoneNumber == phoneNumber){
-                       isUserRegistered = true
-                       return@forEach
-                   }
-                }
-
-            _isRegistered.value = isUserRegistered
+        viewModelScope.launch {
+            _isRegistered.value = SayfeRepository.checkDuplicateRegisteredNumber(phoneNumber)
         }
-
-
-
     }
 
-
-    fun getGuardianAngelsListFromDb(currentUserid: String) {
-        _guardianAngelsStatus.value = BUSY
-        collectionReference.document(currentUserid)
-            .get()
-            .addOnSuccessListener {
-                val data = it.toObject(GuardianData::class.java)
-                data?.let { guardianData ->
-                    _guardianLiveData.value = guardianData
-                    _guardianLiveData.value?.guardianInfo?.let { guardianList->
-                        if (guardianList.isEmpty()){
-                            _guardianAngelsStatus.value = EMPTY
-                        }else{
-                            _guardianAngelsStatus.value = PASSED
-                        }
-                    }
-
-                }
+    fun getPhoneBook(){
+        viewModelScope.launch {
+            _userContactsLiveData.value =
+                currentUserid?.let { SayfeRepository.getPhoneBook(getApplication(), it) }
+            if (_userContactsLiveData.value?.isNotEmpty() == true){
+                _contactStatus.value = PASSED
+            }else{
+                _contactStatus.value = EMPTY
             }
-            .addOnFailureListener {
+        }
+    }
+
+    fun getGuardianAngels(){
+        viewModelScope.launch {
+            try {
+                _guardianAngelsStatus.value = BUSY
+                _guardianLiveData.value = currentUserid?.let {
+                    SayfeRepository.getGuardianAngelsListFromDb(it) }
+                if (_guardianLiveData.value?.guardianInfo?.isEmpty() == true){
+                    _guardianAngelsStatus.value = EMPTY
+                }else{
+                    _guardianAngelsStatus.value = PASSED
+                }
+            }catch (e:Exception){
                 _guardianAngelsStatus.value = FAILED
             }
-    }
-
-    private fun getGetCountryCode(currentUserid: String) {
-
-        userReference.whereEqualTo("currentUserId",currentUserid)
-            .get()
-            .addOnSuccessListener { querySnapshot ->
-                querySnapshot.forEach { queryDocumentSnapshot ->
-                    val users = queryDocumentSnapshot.toObject(Users::class.java)
-                    _currentUserCountryCode.value = users.countryCode
-                    Log.d("COUNTRY CODE FROM METHOD", "getGetCountryCode: ${_currentUserCountryCode.value} ")
-                }
-            }
-
-    }
-
-
-    fun getImageUriFromDb(currentUserid: String){
-    val storageRef = FirebaseStorage.getInstance().getReference();
-
-    val imagesRef: StorageReference = storageRef.child("profile_images").child(currentUserid)
-    imagesRef.downloadUrl.addOnSuccessListener {
-
-        _userImageUri.value = it
-    }.addOnFailureListener {
-
-    }
-
-}
-
-
-//    @SuppressLint("Range")
-//      fun getPhoneBook() : MutableLiveData<MutableList<RecipientContact>?> {
-//        //set default state
-//        _contactStatus.value = BUSY
-//        viewModelScope.launch {
-//            val contacts : MutableList<RecipientContact> = ArrayList()
-//
-//            val cr = getApplication<Application>().contentResolver
-//            val query = cr.query(
-//                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-//                null,
-//                null,
-//                null,
-//                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-//            if (query!!.count > 0){
-//                while (query.moveToNext()){
-//                    val id = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NAME_RAW_CONTACT_ID))
-//                    val name = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
-//                    val number = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-//
-//                    contacts.add(RecipientContact(id,name, number))
-//                    val distinctContact = contacts.distinctBy {
-//                        it.id
-//                    }
-//                    _userContactsLiveData.value = distinctContact as MutableList<RecipientContact>
-//                    _contactStatus.value = PASSED
-//                }
-//            }
-//        }
-//        return _userContactsLiveData
-//
-//    }
-
-    @SuppressLint("Range")
-    fun getPhoneBook(context: Activity) {
-        val user = fAuth.currentUser
-        var countryCode : String? = ""
-
-        viewModelScope.launch {
-            val currentUserid = user?.uid
-            _userContactsLiveData.value  = ContactsState.Empty
-            _contactStatus.value = BUSY
-            //delay(100)
-            userReference.whereEqualTo("currentUserId",currentUserid)
-                .get()
-                .addOnSuccessListener {
-                    for(snapshot : QueryDocumentSnapshot in it){
-                        val doc = snapshot.toObject(Users::class.java)
-                       countryCode = doc.countryCode
-                        val contacts : MutableList<PhonebookContact> = ArrayList()
-                        val cr = context.contentResolver
-                        val query = cr.query(
-                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                            null,
-                            null,
-                            null,
-                            ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-                        if (query != null) {
-                            if (query.count > 0){
-                                while (query.moveToNext()){
-                                    val id = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NAME_RAW_CONTACT_ID))
-                                    val name = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
-                                    var number = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-
-                                    if (number.startsWith("+")){
-                                        number = query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
-                                    }else{
-                                        number = "+$countryCode${query.getString(query.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))}"
-                                    }
-                                    contacts.add(PhonebookContact(id,name, number))
-
-                                    val distinctContact = contacts.distinctBy { distinct ->
-                                        distinct.id
-                                    }
-                                    if (distinctContact.isEmpty()){
-                                        _contactStatus.value = EMPTY
-                                    }else{
-                                        _contactStatus.value = PASSED
-                                    }
-
-
-                                    _userContactsLiveData.value = ContactsState.Success(distinctContact as MutableList<PhonebookContact>)
-
-                                }
-                                query.close()
-                            }
-                        }
-
-                    }
-                }.addOnFailureListener {
-                    Log.d("TAG", "getPhoneBook: ${it.message.toString()}")
-                }
-
-
 
         }
-
     }
 
-    
+    fun incomingDataList(){
+        _incomingDataStatus.value = BUSY
+        viewModelScope.launch {
+            try {
+                _incomingAlertListLiveData.value = currentUserid?.let { SayfeRepository.getIncomingAlertList(it) }
+                if (_incomingAlertListLiveData.value?.isEmpty() == true){
+                    _incomingDataStatus.value = EMPTY
+                }else{
+                    _incomingDataStatus.value = PASSED
+                }
+            }catch (e:Exception){
+                _incomingDataStatus.value = FAILED
+            }
+
+        }
+    }
+
+    fun outgoingDataList(){
+        _outgoingDataStatus.value = BUSY
+        viewModelScope.launch {
+            try {
+                _outgoingAlertListLiveData.value = currentUserid?.let { SayfeRepository.getOutgoingAlertList(it) }
+                if (_outgoingAlertListLiveData.value?.isEmpty() == true){
+                    _outgoingDataStatus.value = EMPTY
+                }else{
+                    _outgoingDataStatus.value = PASSED
+                }
+            }catch (e:Exception){
+                _outgoingDataStatus.value = FAILED
+            }
+
+        }
+    }
 
 }
