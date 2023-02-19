a36feae2dcdf3b89718f8dfe81b7ee884ca3ccec
Akanbi Rahmon <23046879+Rahmon231@users.noreply.github.com>
2023-02-19 17:59:16 +0000

    modified the phonebook fragment

--- app/src/main/java/com/lemzeeyyy/sayfe/fragments/PhoneBookFragment.kt
+++ app/src/main/java/com/lemzeeyyy/sayfe/fragments/PhoneBookFragment.kt
@@ -1,16 +1,23 @@
 package com.lemzeeyyy.sayfe.fragments
 
+import android.Manifest.permission.READ_CONTACTS
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.addCallback
+import androidx.browser.customtabs.CustomTabsClient.getPackageName
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.activityViewModels
@@ -41,85 +48,54 @@ const val REQUEST_CONTACT = 10
 class PhoneBookFragment : Fragment(), CheckedContactListener {
 
     private var contactList = emptyList<PhonebookContact>()
-
     private val viewModel: MainActivityViewModel by activityViewModels()
-
     private lateinit var binding : FragmentPhoneBookBinding
     private lateinit var adapter : PhonebookRecyclerAdapter
     private lateinit var fAuth: FirebaseAuth
     private val database = Firebase.firestore
+    private lateinit var backPressedCallback: OnBackPressedCallback
     private val collectionReference = database.collection("Guardian Angels")
-    private val usersReference = database.collection("Users")
-
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         binding =  FragmentPhoneBookBinding.inflate(inflater, container, false)
+        backPressedCallback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+            findNavController().navigate(PhoneBookFragmentDirections.actionNavPhonebookToNavHome())
+        }
+        backPressedCallback.isEnabled = true
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.allowPermissionBtn.setOnClickListener {
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+            val uri = Uri.fromParts("package", requireContext().packageName, null);
+            intent.setData(uri);
+            startActivity(intent);
+        }
+
         fAuth = Firebase.auth
+
         adapter = PhonebookRecyclerAdapter(this,requireContext())
-        viewModel.getPhoneBook(requireActivity())
 
         binding.addGuardianAngel.setOnClickListener {
             adapter.triggerCheckedListInterface()
         }
+
         binding.allContactsRecycler.adapter = adapter
+        requestContactPermission()
 
-        val readContactPermission = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_CONTACTS)
+    }
 
-
-        if (readContactPermission == PackageManager.PERMISSION_GRANTED )
-        {
-            viewModel.contactStatus.observe(viewLifecycleOwner){
-                updateContactView(it)
-            }
-            viewModel.userContactsLiveDataList.observe(viewLifecycleOwner) {
-                when(it){
-                    is ContactsState.Success ->{
-                                contactList = it.contacts
-                                adapter.updatePhonebookData(contactList.distinctBy { recipientData->
-                                    recipientData.name
-                                })
-                    }
-                    else -> Unit
-
-                }
-
-
-            }
-
-            binding.searchContactId.addTextChangedListener{substring->
-                if (substring.toString().isBlank()){
-                    adapter.updatePhonebookData(contactList.distinctBy { distinctContact->
-                        distinctContact.number
-                    })
-                    return@addTextChangedListener
-                }
-                val filteredContacts = contactList.filter {
-                    it.name.contains(substring.toString(),true)
-                }
-                adapter.updatePhonebookData(filteredContacts)
-
-            }
-        }
-        else {
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.WRITE_CONTACTS,
-                android.Manifest.permission.READ_CONTACTS),
-                PERMISSION_REQUEST
-            )
-        }
-
-
+    private fun updateViewForNoPermission(){
+        binding.phoneBookEmptyState.visibility = View.VISIBLE
+        binding.searchContactTvId.visibility = View.INVISIBLE
+        binding.addGuardianAngel.visibility = View.INVISIBLE
+        binding.allowPermissionBtn.visibility = View.VISIBLE
     }
 
     private fun updateContactView(contactState: Int?) {
@@ -129,37 +105,37 @@ class PhoneBookFragment : Fragment(), CheckedContactListener {
         when(contactState){
             BUSY ->{
                 binding.phoneBookEmptyState.visibility = View.INVISIBLE
-                //binding.phoneBookLoadingState.visibility = View.VISIBLE
-                binding.phonebookShimmer.visibility  = View.VISIBLE
-                binding.phonebookShimmer.stopShimmer()
                 binding.addGuardianAngel.visibility = View.INVISIBLE
                 binding.allContactsRecycler.visibility = View.INVISIBLE
                 binding.searchContactTvId.visibility = View.INVISIBLE
                 binding.phoneBookSavingState.visibility = View.INVISIBLE
                 binding.phoneBookFailedState.visibility = View.INVISIBLE
+                binding.allowPermissionBtn.visibility = View.INVISIBLE
+                binding.emptyStateImagee.visibility = View.GONE
             }
             EMPTY ->{
-                binding.phoneBookEmptyState.visibility = View.VISIBLE
+                binding.emptyStateImagee.visibility = View.VISIBLE
                 binding.phonebookShimmer.visibility  = View.INVISIBLE
                 binding.phonebookShimmer.stopShimmer()
-                binding.phoneBookLoadingState.visibility = View.INVISIBLE
                 binding.addGuardianAngel.visibility = View.INVISIBLE
                 binding.allContactsRecycler.visibility = View.INVISIBLE
                 binding.searchContactTvId.visibility = View.INVISIBLE
                 binding.phoneBookSavingState.visibility = View.INVISIBLE
                 binding.phoneBookFailedState.visibility = View.INVISIBLE
+                binding.allowPermissionBtn.visibility = View.INVISIBLE
             }
 
             PASSED->{
                 binding.phoneBookEmptyState.visibility = View.INVISIBLE
-                binding.phoneBookLoadingState.visibility = View.INVISIBLE
                 binding.addGuardianAngel.visibility = View.VISIBLE
                 binding.allContactsRecycler.visibility = View.VISIBLE
                 binding.searchContactTvId.visibility = View.VISIBLE
                 binding.phoneBookSavingState.visibility = View.INVISIBLE
                 binding.phoneBookFailedState.visibility = View.INVISIBLE
                 binding.phonebookShimmer.visibility  = View.INVISIBLE
+                binding.allowPermissionBtn.visibility = View.INVISIBLE
                 binding.phonebookShimmer.stopShimmer()
+                binding.emptyStateImagee.visibility = View.GONE
             }
 
 
@@ -172,9 +148,7 @@ class PhoneBookFragment : Fragment(), CheckedContactListener {
         val currentUserId = user?.uid
         if (currentUserId != null) {
             binding.phoneBookSavingState.visibility = View.VISIBLE
-            binding.phoneBookLoadingState.visibility = View.INVISIBLE
             binding.phoneBookEmptyState.visibility = View.INVISIBLE
-            binding.phoneBookLoadingState.visibility = View.INVISIBLE
             binding.addGuardianAngel.visibility = View.INVISIBLE
             binding.allContactsRecycler.visibility = View.INVISIBLE
             binding.searchContactTvId.visibility = View.INVISIBLE
@@ -189,7 +163,6 @@ class PhoneBookFragment : Fragment(), CheckedContactListener {
                             .addOnSuccessListener {
                                 binding.phoneBookFailedState.visibility = View.INVISIBLE
                                 binding.phoneBookEmptyState.visibility = View.INVISIBLE
-                                binding.phoneBookLoadingState.visibility = View.INVISIBLE
                                 binding.addGuardianAngel.visibility = View.VISIBLE
                                 binding.allContactsRecycler.visibility = View.VISIBLE
                                 binding.searchContactTvId.visibility = View.VISIBLE
@@ -201,7 +174,6 @@ class PhoneBookFragment : Fragment(), CheckedContactListener {
                             .addOnFailureListener {
                                 binding.phoneBookFailedState.visibility = View.VISIBLE
                                 binding.phoneBookEmptyState.visibility = View.INVISIBLE
-                                binding.phoneBookLoadingState.visibility = View.INVISIBLE
                                 binding.addGuardianAngel.visibility = View.INVISIBLE
                                 binding.allContactsRecycler.visibility = View.INVISIBLE
                                 binding.searchContactTvId.visibility = View.INVISIBLE
@@ -221,81 +193,97 @@ class PhoneBookFragment : Fragment(), CheckedContactListener {
     }
 
     private fun requestContactPermission() {
-
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == PERMISSION_REQUEST) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                viewModel.getPhoneBook(requireActivity())
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED -> {
                 viewModel.contactStatus.observe(viewLifecycleOwner){
                     updateContactView(it)
                 }
-                viewModel.userContactsLiveDataList.observe(viewLifecycleOwner) {
-                    when(it){
-                        is ContactsState.Success ->{
-                            contactList = it.contacts
-                            adapter.updatePhonebookData(contactList.distinctBy { recipientData->
-                                recipientData.name
-                            })
+                viewModel.getPhoneBook()
+                viewModel.userContactsLiveDataList.observe(viewLifecycleOwner){
+                    if (it != null) {
+
+                        contactList = it.distinctBy { distinctById->
+                            distinctById.id
                         }
-                        else -> Unit
+                        adapter.updatePhonebookData(contactList)
 
+                        binding.searchContactId.addTextChangedListener{substring->
+                            if (substring.toString().isBlank()){
+                                adapter.updatePhonebookData(contactList.distinctBy { distinctContact->
+                                    distinctContact.number
+                                })
+                                return@addTextChangedListener
+                            }
+                            val filteredContacts = contactList.filter {filtered->
+                                filtered.name.contains(substring.toString(),true)
+                            }
+                            adapter.updatePhonebookData(filteredContacts)
+                        }
                     }
-
-
                 }
-                viewModel.userContactsLiveDataList.observe(viewLifecycleOwner) {
-                    when(it){
+            }
+            shouldShowRequestPermissionRationale(android.Manifest.permission.READ_CONTACTS) -> {
+                updateViewForNoPermission()
+                // In an educational UI, explain to the user why your app requires this
+                // permission for a specific feature to behave as expected, and what
+                // features are disabled if it's declined. In this UI, include a
+                // "cancel" or "no thanks" button that lets the user continue
+                // using your app without granting the permission.
 
-                        is ContactsState.Empty ->{
+            }
+            else -> {
 
-                        }
-                        is ContactsState.Failure ->{
-
-                        }
-
-                        is ContactsState.Success ->{
-                            contactList = it.contacts
-                            adapter.updatePhonebookData(contactList.distinctBy { recipientData->
-                                recipientData.name
-                            })
-
-                        }
-                        else -> Unit
-
-                    }
-
-
-                }
-                binding.searchContactId.addTextChangedListener{substring->
-                    if (substring.toString().isBlank()){
-                        adapter.updatePhonebookData(contactList.distinctBy { distinctContact->
-                            distinctContact.number
-                        })
-                        return@addTextChangedListener
-                    }
-                    val filteredContacts = contactList.filter {
-                        it.name.contains(substring.toString(),true)
-                    }
-                    adapter.updatePhonebookData(filteredContacts)
-
-                }
-            } else {
-                //ask for permission
-                ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.WRITE_CONTACTS,
+                ActivityCompat.requestPermissions(requireActivity(), arrayOf(
                     android.Manifest.permission.READ_CONTACTS),
-                    PERMISSION_REQUEST
-                )
+                    PERMISSION_REQUEST)
             }
         }
     }
 
+    override fun onRequestPermissionsResult
+                (requestCode: Int,
+                 permissions: Array<String>,
+                 grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CONTACT -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    viewModel.contactStatus.observe(viewLifecycleOwner){
+                        updateContactView(it)
+                    }
+                    viewModel.getPhoneBook()
+                    viewModel.userContactsLiveDataList.observe(viewLifecycleOwner){
+                        if (it != null) {
+                            adapter.updatePhonebookData(it)
+                            contactList = it
+                            binding.searchContactId.addTextChangedListener{substring->
+                                if (substring.toString().isBlank()){
+                                    adapter.updatePhonebookData(contactList.distinctBy { distinctContact->
+                                        distinctContact.number
+                                    })
+                                    return@addTextChangedListener
+                                }
+                                val filteredContacts = contactList.filter {filtered->
+                                    filtered.name.contains(substring.toString(),true)
+                                }
+                                adapter.updatePhonebookData(filteredContacts)
+                            }
+                        }
+                    }
+                } else {
+                    updateViewForNoPermission()
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
+    }
     override fun onContactClick(contacts: MutableList<PhonebookContact>,dbContacts : MutableList<PhonebookContact>) {
         Log.d("TAG", "onContactClick: Triggered")
         dbContacts.forEach {
@@ -313,67 +301,7 @@ class PhoneBookFragment : Fragment(), CheckedContactListener {
 
     override fun onResume() {
         super.onResume()
-        val readContactPermission = ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_CONTACTS)
-
-        if (readContactPermission == PackageManager.PERMISSION_GRANTED )
-        {
-          //  Toast.makeText(requireContext(),"Permission granted",Toast.LENGTH_SHORT).show()
-            viewModel.getPhoneBook(requireActivity())
-            viewModel.contactStatus.observe(viewLifecycleOwner){
-                updateContactView(it)
-            }
-            viewModel.userContactsLiveDataList.observe(viewLifecycleOwner) {
-                when(it){
-                    is ContactsState.Success ->{
-                        contactList = it.contacts
-                        adapter.updatePhonebookData(contactList.distinctBy { recipientData->
-                            recipientData.name
-                        })
-                    }
-                    else -> Unit
-
-                }
-
-
-            }
-            viewModel.getPhoneBook(requireActivity())
-            viewModel.userContactsLiveDataList.observe(viewLifecycleOwner) {
-                when(it){
-                    is ContactsState.Empty ->{
-
-                    }
-                    is ContactsState.Success ->{
-                        contactList = it.contacts
-                        adapter.updatePhonebookData(contactList.distinctBy { recipientData->
-                            recipientData.name
-                        })
-
-                    }
-                    else -> Unit
-
-                }
-
-
-            }
-            binding.searchContactId.addTextChangedListener{substring->
-                if (substring.toString().isBlank()){
-                    adapter.updatePhonebookData(contactList.distinctBy { distinctContact->
-                        distinctContact.number
-                    })
-                    return@addTextChangedListener
-                }
-                val filteredContacts = contactList.filter {
-                    it.name.contains(substring.toString(),true)
-                }
-                adapter.updatePhonebookData(filteredContacts)
-
-            }
-        } else {
-            ActivityCompat.requestPermissions(requireActivity(), arrayOf(android.Manifest.permission.WRITE_CONTACTS,
-                android.Manifest.permission.READ_CONTACTS),
-                PERMISSION_REQUEST
-            )
-        }
+        requestContactPermission()
     }
 
 
